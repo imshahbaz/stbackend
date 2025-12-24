388cff5e1b5122438bbe004713b0c2dfc9280e28
imshahbaz21 <imshahbaz21@gmail.com>
2025-12-24 23:04:54 +0530

    fix

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -37,30 +37,39 @@ func (ctrl *UserController) RegisterRoutes(router *gin.RouterGroup) {
 // @Accept       json
 // @Produce      json
 // @Param        update  body      model.UserDto  true  "Target Email and New Username"
-// @Success      200     {object}  model.UserDto
-// @Failure      400     {object}  map[string]string
-// @Failure      404     {object}  map[string]string
+// @Success      200     {object}  model.Response
+// @Failure      400     {object}  model.Response
+// @Failure      401     {object}  model.Response
+// @Failure      500     {object}  model.Response
 // @Router       /user/username [patch]
 func (ctrl *UserController) UpdateUsername(c *gin.Context) {
 	var req model.UserDto
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON payload"})
+		c.JSON(http.StatusBadRequest, model.Response{
+			Success: false,
+			Error:   "Invalid Request",
+		})
 		return
 	}
 
 	// 1. Call Service (Returns updated *model.User)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	updatedUser, err := ctrl.userSvc.UpdateUsername(ctx, req.Email, req.Username)
+	_, err := ctrl.userSvc.UpdateUsername(ctx, req.Email, req.Username)
 	if err != nil {
-		// Handle case where user might not exist or DB error
-		c.JSON(http.StatusNotFound, gin.H{"error": "User not found or update failed"})
+		c.JSON(http.StatusNotFound, model.Response{
+			Success: false,
+			Error:   "User not found or update failed",
+		})
 		return
 	}
 
 	// 2. Return the DTO (React will use this to update its state)
-	c.JSON(http.StatusOK, updatedUser.ToDto())
+	c.JSON(http.StatusOK, model.Response{
+		Success: true,
+		Message: "Username Updated",
+	})
 }
 
 // UpdateTheme godoc
