e69515cd5efb558cd800404b51ecf483060319d7
imshahbaz21 <imshahbaz21@gmail.com>
2025-12-31 23:59:27 +0530

    fix

--- model/config.go
+++ model/config.go
@@ -1,21 +1,23 @@
 package model
 
 type MongoEnvConfig struct {
-	DebugMode     bool     `bson:"debugMode" json:"debugMode"`
-	Environment   string   `bson:"environment" json:"environment"`
-	Port          string   `bson:"port" json:"port"`
-	MongoUri      string   `bson:"mongoUri" json:"mongoUri"`
-	JwtSecret     string   `bson:"jwtSecret" json:"jwtSecret"`
-	BrevoApiKey   string   `bson:"brevoApiKey" json:"brevoApiKey"`
-	BrevoEmail    string   `bson:"brevoEmail" json:"brevoEmail"`
-	FrontendUrls  []string `bson:"frontendUrls" json:"frontendUrls"`
-	RateLimiter   bool     `bson:"rateLimiter" json:"rateLimiter"`
-	Leverage      float32  `bson:"leverage" json:"leverage"`
-	MongoUser     string   `bson:"mongoUser" json:"mongoUser"`
-	MongoPassword string   `bson:"mongoPassword" json:"mongoPassword"`
+	ID           string   `json:"-" bson:"_id,omitempty"`
+	FrontendUrls []string `json:"frontendUrls" bson:"frontendUrls"`
+	BrevoEmail   string   `json:"brevoEmail" bson:"brevoEmail"`
+	BrevoApiKey  string   `json:"brevoApiKey" bson:"brevoApiKey"`
+	ApiKey       string   `json:"apiKey" bson:"apiKey"`
+	Leverage     float32  `json:"leverage" bson:"leverage"`
+	DebugMode    bool     `json:"debugMode" bson:"debugMode"`
+	RateLimiter  bool     `json:"rateLimiter" bson:"rateLimiter"`
+	JwtSecret    string   `json:"jwtSecret" bson:"jwtSecret"`
 }
 
-type EnvConfig = MongoEnvConfig
+type EnvConfig struct {
+	Port          string `json:"port"`
+	MongoUser     string `json:"mongoUser"`
+	MongoPassword string `json:"mongoPassword"`
+	Environment   string `json:"environment"`
+}
 
 // --- Huma Structs ---
 
