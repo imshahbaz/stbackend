fb8305afe2370f5952b7b0e760de7ef06f8c717c
imshahbaz21 <imshahbaz21@gmail.com>
2026-01-25 22:47:45 +0530

    using resty

--- service/angel_one_service.go
+++ service/angel_one_service.go
@@ -8,89 +8,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/go-resty/resty/v2"
 	"github.com/pquerna/otp/totp"
 	"github.com/rs/zerolog/log"
 )
-
-// import (
-// 	"backend/cache"
-// 	"backend/database"
-// 	"backend/model"
-// 	"backend/util"
-// 	"time"
-
-// 	"fmt"
-
-// 	SmartApi "github.com/angel-one/smartapigo"
-// 	"github.com/pquerna/otp/totp"
-// 	"github.com/rs/zerolog/log"
-// )
-
-// var (
-// 	angelOneClient *SmartApi.Client
-// )
-
-// type AngelOneService interface {
-// 	RefreshBrokerSession() error
-// 	GetLTP(tradingSymbol, symbolToken string) (float64, error)
-// }
-
-// type AngelOneServiceImpl struct {
-// 	angelOneConfig *model.AngelOneConfig
-// }
-
-// func NewAngelOneService(angelOneConfig *model.AngelOneConfig) AngelOneService {
-// 	return &AngelOneServiceImpl{angelOneConfig: angelOneConfig}
-// }
-
-// func GenerateTOTP(secret string) (string, error) {
-// 	code, err := totp.GenerateCode(secret, time.Now())
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return code, nil
-// }
-
-// func (s *AngelOneServiceImpl) RefreshBrokerSession() error {
-// 	otp, _ := GenerateTOTP(s.angelOneConfig.Seed)
-
-// 	angelOneClient = SmartApi.New(s.angelOneConfig.ClientID, s.angelOneConfig.Password, s.angelOneConfig.ApiKey)
-
-// 	var accessToken string
-// 	database.RedisHelper.GetAsStruct("broker_access_token", &accessToken)
-
-// 	if accessToken != "" {
-// 		angelOneClient.SetAccessToken(accessToken)
-// 		return nil
-// 	}
-
-// 	session, err := angelOneClient.GenerateSession(otp)
-
-// 	if err == nil {
-// 		cache.GoSet("broker_access_token", session.AccessToken, util.ZerodhaTokenExpiry())
-// 		log.Info().Msg("Broker session refreshed successfully")
-// 		return nil
-// 	} else {
-// 		log.Error().Msg("Broker session refresh failed")
-// 		return err
-// 	}
-// }
-
-// func (s *AngelOneServiceImpl) GetLTP(tradingSymbol, symbolToken string) (float64, error) {
-// 	if angelOneClient == nil {
-// 		return 0, fmt.Errorf("angelOneClient not initialized, please refresh session first")
-// 	}
-// 	ltpResponse, err := angelOneClient.GetLTP(SmartApi.LTPParams{
-// 		Exchange:      "NSE",
-// 		TradingSymbol: tradingSymbol + "-EQ",
-// 		SymbolToken:   symbolToken,
-// 	})
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	return ltpResponse.Ltp, nil
-// }
 
 type AngelOneService interface {
 	RefreshBrokerSession() error
@@ -116,7 +38,10 @@ func NewAngelOneService(angelOneConfig *model.AngelOneConfig) AngelOneService {
 		SetHeader("X-MACAddress", "00:00:00:00:00:00").
 		SetTimeout(10 * time.Second).
 		SetRetryCount(2).
-		SetRetryWaitTime(1 * time.Second)
+		SetRetryWaitTime(1 * time.Second).
+		SetJSONMarshaler(sonic.ConfigDefault.Marshal).
+		SetJSONUnmarshaler(sonic.ConfigDefault.Unmarshal)
+
 	return &AngelOneServiceImpl{angelOneConfig: angelOneConfig, restyClient: client}
 }
 
